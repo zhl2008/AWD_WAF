43588bffcb22d7f1a70f393b30e48d90c221f0d5
zhl2008 <zhl2008vvvss@gmail.com>
2018-06-16 13:08:40 +0800

    update the basic rules

--- my_waf/waf.php
+++ my_waf/waf.php
@@ -1,4 +1,4 @@
-<?php
+<?p
 /*
 
     @author Hence/Lancet
@@ -89,7 +89,7 @@ function waf(){
     $input = array("Get"=>$get, "Post"=>$post, "Cookie"=>$cookie, "File"=>$files, "Header"=>$header);
     // the filter rules
     $pattern = "select|insert|update|delete|union|load_file|outfile|dumpfile|sub|hex";
-    $pattern .= "|file_put_contents|fwrite|curl|system|eval|assert";
+    $pattern .= "|file_put_contents|fwrite|curl|system|eval|assert|flag";
     $pattern .="|passthru|exec|system|chroot|scandir|chgrp|chown|shell_exec|proc_open|proc_get_status|popen|ini_alter|ini_restore";
     $pattern .="|`|openlog|syslog|readlink|symlink|popepassthru|stream_socket_server|assert|pcntl_exec";
     $vpattern = explode("|",$pattern);
